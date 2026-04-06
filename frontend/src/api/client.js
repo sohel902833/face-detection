0cadf30fa6a6daadea9e39e37efbe96825ed2203
Sohrab <sohrab@manush.tech>
2026-04-07 02:10:30 +0600

    updated response interceptors

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -20,11 +20,13 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (res) => res,
   (err) => {
-    // if (err.response?.status === 401) {
-    //   localStorage.removeItem('fa_token');
-    //   localStorage.removeItem('fa_user');
-    //   window.location.href = '/login';
-    // }
+    const requestUrl = err.config.url;
+    const isLoginUrl = requestUrl.includes("/auth/login");
+    if (!isLoginUrl && err.response?.status === 401) {
+      localStorage.removeItem("fa_token");
+      localStorage.removeItem("fa_user");
+      window.location.href = "/login";
+    }
     return Promise.reject(err);
   },
 );
